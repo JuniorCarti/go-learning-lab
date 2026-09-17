55d7f7b32fa4c0509804bfdea1c71050e0d82f4e
Ridge Junior <ridgejunior204@gmail.com>
2026-09-17 17:42:04 +0300

    exercise-01-basic constant

---
+++ basics/constants/exercise-01-basic-constant/main.go
@@ -0,0 +1,33 @@
+/*Exercise 1 — Basic constant
+
+Create:
+
+basics/constants/exercise-01-basic-constant/main.go
+
+Create a constant named:
+
+PI
+
+with value:
+
+3.14
+
+Then print it.
+
+Expected:
+
+3.14
+
+Requirement:
+
+const PI = ...*/
+
+package main
+
+import "fmt"
+
+const PI = 3.14
+
+func main() {
+	fmt.Println(PI)
+}
