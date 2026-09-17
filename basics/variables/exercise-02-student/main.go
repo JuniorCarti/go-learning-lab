a5ceb83e49bef8a7e471cb5cfd1715bf5189a698
Ridge Junior <ridgejunior204@gmail.com>
2026-09-17 15:26:59 +0300

    exercise-02-student

---
+++ basics/variables/exercise-02-student/main.go
@@ -0,0 +1,15 @@
+//the output should be 
+//Ridge
+//27
+
+package main
+
+import "fmt"
+
+func main() {
+	var studentName string = "Ridge"
+	var studentAge int = 27
+
+	fmt.Println(studentName)
+	fmt.Println(studentAge)
+}
