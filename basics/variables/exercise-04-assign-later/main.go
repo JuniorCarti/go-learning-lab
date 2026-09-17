41654f5d863e5177bebf60279147fdb8f68c28bf
Ridge Junior <ridgejunior204@gmail.com>
2026-09-17 15:43:07 +0300

    exercise-04-assign-later

---
+++ basics/variables/exercise-04-assign-later/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var language string
+	var studentName string
+	studentName = "John"
+	language = "Go"
+
+	fmt.Printf("Hello, %s is learning %s\n", studentName, language)
+}
+
+//fmt.Printf("hello, %s is learning %s\n", studentName, language)
