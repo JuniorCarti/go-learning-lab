8259eca66e316e1a59d04a2a8a4dff3ce7260f23
Ridge Junior <ridgejunior204@gmail.com>
2026-09-17 14:40:23 +0300

    exercise-01-hello-world

---
+++ basics/syntax/exercise-01-hello-world/main.go
@@ -0,0 +1,14 @@
+//This file belongs to an executable program.
+package main
+
+//brings in Go's fmt package, which contains functions for displaying formatted output.
+import "fmt"
+
+//this is where the program starts running.
+
+func main() {
+   // fmt        → use the fmt package
+    // Println    → print something and then move to a new line
+    // "Hello, World!" → the string we want to print
+	fmt.Println("Hello, World!")
+}
