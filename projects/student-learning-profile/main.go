1e559c9e53908fb43fb566d48b164169d0a03009
Ridge Junior <ridgejunior204@gmail.com>
2026-09-17 17:28:23 +0300

    student-learning profile project

--- projects/student-learning-profile/main.go
+++ projects/student-learning-profile/main.go
@@ -31,20 +31,28 @@ func main() {
 	var bonusPoints int
 
 	fmt.Println("===== STUDENT LEARNING PROFILE =====")
+
+	fmt.Println()
 	fmt.Println("Country:", country)
 	fmt.Println("Repository:", repository)
+
+	fmt.Println("===================================")
 	fmt.Println("Name:", firstName, lastName)
 	fmt.Println("Exercises Completed:", exercisesCompleted)
 	fmt.Println("Projects Completed:", projectsCompleted)
 	fmt.Println("Age:", age)
 	fmt.Println("Platform:", platform)
 	fmt.Println("City:", city)
+
+	fmt.Println("===================================")
 	fmt.Println("Course:", course)
 	fmt.Println("Learning:", learning)
 	fmt.Println("Current Topic:", currentTopic)
 	fmt.Println("Score:", score)
 	fmt.Println("Target Score:", targetScore)
 	fmt.Println("Language:", language)
+
+	fmt.Println("===================================")
 	fmt.Println("Certificate Name:", certificateName)
 	fmt.Println("Account Verified:", accountVerified)
 	fmt.Println("Certificate Earned:", certificateEarned)
